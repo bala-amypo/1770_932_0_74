d68953a223e9f038a250b087229b3b56cfa8625a
bala-amypo <bala@amypo.in>
2025-12-19 05:19:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,56 +1,65 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.springframework.validation.annotation.Validated;
 
 @Entity
+@Validated
 public class StudentEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-     @NotBlank
+
+    @NotBlank
     private String name;
 
     @NotBlank
     @Email
     private String email;
 
-    @DecimalMin(value = "0.0")
-    @DecimalMax(value = "10.0")
+    @DecimalMin("0.0")
+    @DecimalMax("10.0")
     private float cgpa;
 
     public StudentEntity(
             @NotBlank String name,
-            @Email @NotBlank String email,
+            @NotBlank @Email String email,
             @DecimalMin("0.0") @DecimalMax("10.0") float cgpa
     ) {
         this.name = name;
         this.email = email;
         this.cgpa = cgpa;
     }
-    public StudentEntity(){
 
+    public StudentEntity() {}
+
+    public Long getId() {
+        return id;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public void setEmail(String email){
+
+    public void setEmail(String email) {
         this.email = email;
     }
-    public void setCgpa(float cgpa){
+
+    public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
     }
-    public String getName(){
-        return this.name;
-    }
-    public String getEmail(){
-        return this.email;
-    }
-    public float getCgpa(){
-        return this.cgpa;
-    }
-    
 
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public float getCgpa() {
+        return cgpa;
+    }
 }
