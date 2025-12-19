3f5822c50af2578c20ee3d64ed0d756fedc37c4d
bala-amypo <bala@amypo.in>
2025-12-19 05:18:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,16 +3,11 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;   
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentService;
-
 
 @RestController
 public class StudentController {
@@ -21,15 +16,16 @@ public class StudentController {
     StudentService ser;
 
     @PostMapping("/addStudents")
-    public StudentEntity addStudents(@RequestBody StudentEntity student){
+    public StudentEntity addStudents(
+            @Valid @RequestBody StudentEntity student) {
         return ser.addStudents(student);
     }
-    
+
     @GetMapping("/getStudents")
     public List<StudentEntity> getStudents() {
         return ser.getStudents();
     }
-    
+
     @GetMapping("/getStudent/{id}")
     public StudentEntity getStudentById(@PathVariable Long id) {
         return ser.getStudentById(id);
@@ -38,12 +34,11 @@ public class StudentController {
     @DeleteMapping("/deleteStudent/{id}")
     public String deleteStudentById(@PathVariable Long id) {
         StudentEntity student = ser.getStudentById(id);
-        if(student!=null) {
+        if (student != null) {
             ser.deleteStudentById(id);
             return "Student deleted successfully.";
         } else {
             return "Student not found.";
         }
     }
-
 }
